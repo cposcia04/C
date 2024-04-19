a3c3b4c08b34a134aca170b7b4881c9a23ba51dc
Camilla Poscia <c.poscia@studenti.unipi.it>
2024-04-19 22:21:21 +0200

    Create ex4.c

---
+++ Exercises/ex4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    int counter = 1;
+    double grade;
+    double total = 0; 
+
+    while (counter <= 10) {
+        printf("%s", "Enter a grade: ");
+        scanf("%lf", &grade);
+        total += grade; // 
+        ++counter;
+    }
+
+    double average = total / 10; 
+    printf("Class average is: %.2f\n", average);
+
+    return 0;
+}
